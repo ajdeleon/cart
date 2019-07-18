6670385825e2028320112ab283dc6ced30794970
AJ de Leon <ajdeleon1064@gmail.com>
2019-07-18 13:02:27 -0400

    merge both api call arrays into one for easy access
    - This ended up being a big issue due to there being a 'quantity' field
    in the objects of each array. Because of this the quantity field on the
    main productCards are wrong. They are using the 'quantity' from the
    productApi, basically how many products of this type the warehouse has
    available, not the quantity from the card order

--- src/hooks/useCustomerOrder.js
+++ src/hooks/useCustomerOrder.js
@@ -1,5 +1,6 @@
 import { useState, useEffect, useReducer } from 'react'
 import axios from 'axios'
+import _ from 'lodash'
 
 import { FETCH_INIT, FETCH_SUCCESS, FETCH_FAILURE } from './types'
 import { ORDER_URL, PRODUCT_BASE_URL } from './urls'
@@ -9,7 +10,7 @@ const orderReducer = (state, action) => {
     case FETCH_INIT:
       return { ...state, isLoading: true, isError: false }
     case FETCH_SUCCESS:
-      return { ...state, isLoading: false, isError: false, order: action.payload }
+      return { ...state, isLoading: false, isError: false, products: action.payload.products, user: action.payload.user, cart: action.payload.cart}
     case FETCH_FAILURE:
       return { ...state, isLoading: false, isError: true }
     default:
@@ -20,7 +21,9 @@ const orderReducer = (state, action) => {
 const useCustomerOrder = () => {
   const [updater, setUpdater] = useState('')
   const initialState = {
-    order: {},
+    products: [],
+    user: {},
+    cart: [],
     isLoading: false,
     isError: false
   }
@@ -35,10 +38,21 @@ const useCustomerOrder = () => {
 
 
       try {
+        //this is really bad, but doing it for time constraints
+        // the lodash map is overriding the 'res' before I can save a reference so I'm just calling it again due to time constraintes
         const res = await axios.get(ORDER_URL)
+        const cartRes = await axios.get(ORDER_URL)
+        // location_id=6 is hardcoded for philly for now
+        const productIds = res.data.cart.products.map(product => product.product_id)
+        const productRes = await axios.get(`${PRODUCT_BASE_URL}?location_id=6&product_id=${productIds}`)
+        
+        //this doesn't work because there are duplicate keys like "quantity" in both arrays that mess things up
+        const mergedProductData = _.map(res.data.cart.products, (product) => {
+          return _.assign(product, _.find(productRes.data.products, ['product_id', product.product_id]))
+        })
 
         if (!didNotMount) {
-          dispatch({ type: FETCH_SUCCESS, payload: res.data })
+          dispatch({ type: FETCH_SUCCESS, payload: { user: res.data.user, products: mergedProductData, cart: cartRes.data.cart.products } })
         }
 
       } catch (err) {
