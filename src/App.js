b30a66c6483c53c0388b77cf9764a7f93ed2231c
AJ de Leon <ajdeleon1064@gmail.com>
2019-07-18 10:52:42 -0400

    use api hook to load data and handle loading/error states

--- src/App.js
+++ src/App.js
@@ -1,13 +1,24 @@
 import React from 'react'
 import './App.css'
 
+import useCustomerOrder from './hooks/useCustomerOrder'
+
 function App() {
+  const [{ order, isLoading, isError }] = useCustomerOrder()
+  console.log(order)
   return (
     <main>
       <header className="header__title">goCart</header>
       <section className="cart__main">
         <section className="cart__product-list">
-          product list
+          { isError
+              ? 'There was an error loading your order data. Please try to refresh your page or contact and admin here: admin@goCart.com'
+              : (
+                 isLoading
+                  ? 'loading...'
+                  : 'data loaded'
+              )
+          }
         </section>
         <section className="cart__checkout">
           checkout
